b5ad208fa8eba2db477611ff01396f69af4a3a2a
arronHealy <G00346883@gmit.ie>
2020-05-22 22:52:16 +0100

    set up app, with user controller

---
+++ MobileApp/src/test/java/com/appsdeveloperblog/appws/MobileAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.appws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
