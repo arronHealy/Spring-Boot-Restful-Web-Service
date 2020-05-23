8a109c9c529fd84686b63bab1801c86269013c1c
arronHealy <G00346883@gmit.ie>
2020-05-23 16:45:22 +0100

    added some more classes for user functionality

--- MobileApp/src/main/java/com/appsdeveloperblog/appws/ui/controller/UserController.java
+++ MobileApp/src/main/java/com/appsdeveloperblog/appws/ui/controller/UserController.java
@@ -4,8 +4,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.appsdeveloperblog.appws.ui.model.request.UserDetailsRequestModel;
+import com.appsdeveloperblog.appws.ui.model.response.UserRest;
 
 @RestController
 @RequestMapping("users")
@@ -18,9 +22,9 @@ public class UserController {
 	}
 	
 	@PostMapping
-	public String createUser()
+	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails)
 	{
-		return "create user called";
+		return null;
 	}
 	
 	@PutMapping
